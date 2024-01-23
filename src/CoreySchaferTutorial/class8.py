4927a95948aa32a83db48605da0e1fe259e4cd4a
Clemar Junior de Mattos Piccini <cj.piccini@gmail.com>
2024-01-23 15:00:54 -0300

    class 8

--- src/CoreySchaferTutorial/class8.py
+++ src/CoreySchaferTutorial/class8.py
@@ -1,2 +1,24 @@
 #Functions
 
+def hello_func():
+    return "isso é uma função."
+
+print(hello_func())
+
+#função com argumentos
+
+def hello_func_arg(greeting, name='You'):
+    return '{}, {}'.format(greeting, name)
+
+print(hello_func_arg('Hi', name = 'Junior'))
+
+#funcao um pouco mais complexa
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+courses = ['Math', 'Art']
+info = {'name': 'John', 'age': 22}
+
+student_info(*courses, **info)
