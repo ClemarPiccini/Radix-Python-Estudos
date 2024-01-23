9c419c1e6fe899d7d755261f28c47043b8829bc8
Clemar Junior de Mattos Piccini <cj.piccini@gmail.com>
2024-01-23 14:13:52 -0300

    Design Patterns and class 7

--- src/CoreySchaferTutorial/class7.py
+++ src/CoreySchaferTutorial/class7.py
@@ -1,1 +1,58 @@
 #Loops For/While
+nums = [1, 2, 3, 4, 5, 6]
+print('-----------------------------------------------------------------')
+print('-------------FOR--------------')
+for num in nums:
+    print(num)
+print('-----------------------------------------------------------------')
+print('-------------USANDO continue--------------')
+for num in nums:
+    if num == 3:
+        print('Found!')
+        continue
+    print(num)
+
+print('-----------------------------------------------------------------')
+print('-------------USANDO break--------------')
+for num in nums:
+    if num == 3:
+        print('Found!')
+        break
+    print(num)
+
+print('-----------------------------------------------------------------')
+print('-------------USANDO range--------------')
+
+for i in range(10):
+    print(i)
+
+print('-----------------------------------------------------------------')
+print('-------------lops aninhados--------------')
+
+for num in nums:
+    for letter in 'abc':
+        print(num, letter)
+
+print('-----------------------------------------------------------------')
+print('-------------WHILE--------------')
+
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
+
+
+print('-----------------------------------------------------------------')
+print('-------------USANDO break--------------')
+
+y = 0
+
+while y < 10:
+    if y == 5:
+        print('Metade')
+        break
+    print(y)
+    y += 1
+
+#se fizer um while True sem um break entra em loop infinito
---
+++ src/Design-Pattern/README.md
@@ -0,0 +1,16 @@
+https://refactoring.guru/pt-br/design-patterns/catalog
+
+
+Padrões criacionais:
+
+Estes padrões fornecem vários mecanismos de criação de objetos, que aumentam a flexibilidade e reutilização de código já existente.
+
+
+Padrões estruturais:
+
+Estes padrões explicam como montar objetos e classes em estruturas maiores mas ainda mantendo essas estruturas flexíveis e eficientes
+
+
+Padrões comportamentais:
+
+Estes padrões são voltados aos algoritmos e a designação de responsabilidades entre objetos.
