19a8c94cf1e1d1b18d80f10f467e553bce60d9f2
Clemar <cj.piccini@gmail.com>
2024-02-20 12:59:48 -0300

    class 40

---
+++ src/CoreySchaferTutorial/class40.py
@@ -0,0 +1,23 @@
+#Classes and Instances
+
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+emp_1 = Employee('Junior', 'Piccini', 50000)
+emp_2 = Employee('Teste', 'User', 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullname())
